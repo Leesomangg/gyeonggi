47fc90e77763457d8c8402d6b63d5bd57d0ce834
Lee <smmang@gmail.com>
2024-05-13 17:47:30 +0900

    모달창

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
 $(document).ready(function () {
+  // 모달창 ===========================
+  const body = document.querySelector("body");
+  const modal = document.querySelector(".modal-wrap");
+  modal.addEventListener("click", function () {
+    modal.style.display = "none";
+    fadeout(modal);
+  });
+  //isOpen 값에 따라 스크롤을 제어하는 함수
+  function controlScroll(isOpen) {
+    if (isOpen) {
+      body.style.overflow = "hidden";
+    } else {
+      body.style.overflow = "auto";
+    }
+  }
+  // 초기 모달 상태 설정
+  const isOpen = true;
+  controlScroll(isOpen);
+  modal.addEventListener("click", function () {
+    modal.style.display = "none";
+    // 모달이 닫힐 때는 스크롤을 다시 활성화
+    controlScroll(false);
+  });
   // 우츨 픽스 메뉴
   var fixA = $(".fix-a");
   $.each(fixA, function (index, item) {
